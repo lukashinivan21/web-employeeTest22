1b8288f61c017b6290051629308ede40fa91bc91
Ivan <uvanes21@yandex.ru>
2022-03-29 09:01:28 +0300

    Первый коммит

---
+++ src/main/java/pro/sky/java/lesson24/webemployeetest22/WebEmployeeTest22Application.java
@@ -0,0 +1,13 @@
+package pro.sky.java.lesson24.webemployeetest22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebEmployeeTest22Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebEmployeeTest22Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/lesson24/webemployeetest22/WebEmployeeTest22ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.lesson24.webemployeetest22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebEmployeeTest22ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
